df6b6458da3e8ec0d155a8470a993a3edde66dae
hefepa <paulorimogunje@gmail.com>
2024-05-12 15:17:48 +0100

    Profile UI

---
+++ BeachesAndBoats/Features/Profile/ProfileModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileModel.swift
+//  Beaches and Boats
+//
+//  Created by Paul Orimogunje on 12/05/2024.
+//
+
+import Foundation
+
+struct ProfileModel {
+    let img: String?
+    let title: String?
+}
--- BeachesAndBoats/Resources/Images.swift
+++ BeachesAndBoats/Resources/Images.swift
@@ -12,6 +12,16 @@ enum Assets: String {
     case slide1 = "slide1"
     case slide2 = "slide2"
     case slide3 = "slide3"
+    case more = "formkit_right"
+    case list = "gala_add"
+    case switchicon = "icon-park_switch"
+    case notification = "iconamoon_notification-light"
+    case book = "iconoir_book"
+    case loginSecurity = "mingcute_shield-line"
+    case saved = "ph_heart"
+    case profile = "solar_user-linear"
+    case support = "streamline_customer-support-1"
+    case payment = "uil_money-bill"
 
     
     
