b18d47b83927b5703b1cc5e7036748ff29801170
hefepa <paulorimogunje@gmail.com>
2024-09-23 12:34:53 +0100

    Latest

---
+++ BeachesAndBoats/Features/HomeBeachHouses/PhotoCollectionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoCollectionsViewController.swift
+//  BeachesAndBoats
+//
+//  Created by WEMA on 10/06/2024.
+//
+
+import UIKit
+
+class PhotoCollectionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
